07f1f52455c636997a17c0536b1d27a121a21ec0
uy <a.normurzaev20@gmail.com>
2026-05-06 11:28:32 +0500

    efect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react'
 
-export class App extends Component {
-  static propTypes = {
-
-  }
-
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+const Card = () => {
+  const [data, SetData] = useState ([]);
+  useEffect (()=>{
+    fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Students")
+    .then(data => data.json())
+    .then(data => {
+      SetData(data);
+    })
+  })
+  return (
+    <div>
+      {data.map(el => (
+        <div key = {el.id}>
+          <img src={el.avatar} alt="" srcset="" />
+          <h2>{el.name}</h2>
+        </div>
+      ))}
+    </div>
+  )
 }
 
-export default App
+export default Card
+
+ 
