4a89074f2d53973db4ca69420d5087891636161c
uy <a.normurzaev20@gmail.com>
2026-05-06 11:19:00 +0500

    efect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
 
-import { useEffect, useState } from "react";
 
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class App extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App
